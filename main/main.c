32d5e6203dfd0ade08112bcb992ffc329a283cf6
Severin Lochschmidt <seviron53@gmail.com>
2023-09-05 18:12:55 +0200

    Adapt udp_server example

---
+++ main/main.c
@@ -0,0 +1,79 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "send_codes.c"
+#include "udp_server.c"
+
+TaskHandle_t state_controller_handle = NULL;
+TaskHandle_t transmitter_handle = NULL;
+TaskHandle_t udp_server_handle = NULL;
+
+struct State {
+    bool room_light_on;
+} state;
+
+/**
+ * Main controller of the state machine receives switch requests and sends actions
+ * This task must be started first to initialize shared resources
+ */
+static void state_controller_task(void *pvParameters)
+{
+    state.room_light_on = false;
+    // Wait for other tasks to initialize
+    vTaskDelay(pdMS_TO_TICKS(400));
+    // Send disable light signal to be sure its off
+    xTaskNotify( transmitter_handle, 1, eSetValueWithOverwrite );
+
+    uint32_t message;
+
+    while(1){
+        // Wait for Note from Button or UDP to switch
+        vTaskDelay(pdMS_TO_TICKS(2000)); // debounce
+        ESP_LOGI(TAG, "Controller Standing By.");
+        xTaskNotifyStateClear(NULL);
+        if (xTaskNotifyWait( ULONG_MAX, ULONG_MAX, &message, portMAX_DELAY )){
+            ESP_LOGI(TAG, "Switch Request received.");
+            if(state.room_light_on) message = 1; else message = 0;
+            xTaskNotify( transmitter_handle, message, eSetValueWithOverwrite );
+            state.room_light_on = !state.room_light_on; //Flip
+        }
+    }
+}
+
+/**
+ * Sends a code to the 433Mhz transmitter to switch the lights on or off
+ * 
+ * Sleeps until given a direct notification. Then switches to the binary code in the notification.
+ */
+static void transmitter_task(void *pvParameters)
+{ 
+    uint32_t note;
+    while(1){
+        ESP_LOGI(TAG, "Transmitter Standing By.");
+        if (xTaskNotifyWait( ULONG_MAX, 0, &note, portMAX_DELAY )){
+            if (note == 0){ // If light is off switch it on 
+                ESP_LOGI(TAG, "Sending Light on Signal!");
+                chan1on(20);
+            } else { // If light is on switch it off
+                ESP_LOGI(TAG, "Sending Light off Signal!");
+                chan1off(20);
+            }
+        }
+    }
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    xTaskCreate(state_controller_task, "state_controller", 1024, NULL, 4, &state_controller_handle);
+    xTaskCreate(transmitter_task, "transmitter", 4096, NULL, 3, &transmitter_handle);
+    xTaskCreate(custom_udp_server_task, "udp_server", 4096, (void *) &state_controller_handle, 2, &udp_server_handle);
+}
--- main/udp_server.c
+++ main/udp_server.c
@@ -1,11 +1,3 @@
-/* BSD Socket API Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <string.h>
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
@@ -27,7 +19,7 @@
 
 static const char *TAG = "example";
 
-static void udp_server_task(void *pvParameters)
+static void custom_udp_server_task(void *pvParameters)
 {
     char rx_buffer[128];
     char addr_str[128];
@@ -57,20 +49,6 @@ static void udp_server_task(void *pvParameters)
         }
         ESP_LOGI(TAG, "Socket created");
 
-#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
-        int enable = 1;
-        lwip_setsockopt(sock, IPPROTO_IP, IP_PKTINFO, &enable, sizeof(enable));
-#endif
-
-#if defined(CONFIG_EXAMPLE_IPV4) && defined(CONFIG_EXAMPLE_IPV6)
-        if (addr_family == AF_INET6) {
-            // Note that by default IPV6 binds to both protocols, it is must be disabled
-            // if both protocols used at the same time (used in CI)
-            int opt = 1;
-            setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-            setsockopt(sock, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof(opt));
-        }
-#endif
         // Set timeout
         struct timeval timeout;
         timeout.tv_sec = 10;
@@ -86,30 +64,10 @@ static void udp_server_task(void *pvParameters)
         struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
         socklen_t socklen = sizeof(source_addr);
 
-#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
-        struct iovec iov;
-        struct msghdr msg;
-        struct cmsghdr *cmsgtmp;
-        u8_t cmsg_buf[CMSG_SPACE(sizeof(struct in_pktinfo))];
-
-        iov.iov_base = rx_buffer;
-        iov.iov_len = sizeof(rx_buffer);
-        msg.msg_control = cmsg_buf;
-        msg.msg_controllen = sizeof(cmsg_buf);
-        msg.msg_flags = 0;
-        msg.msg_iov = &iov;
-        msg.msg_iovlen = 1;
-        msg.msg_name = (struct sockaddr *)&source_addr;
-        msg.msg_namelen = socklen;
-#endif
-
         while (1) {
             ESP_LOGI(TAG, "Waiting for data");
-#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
-            int len = recvmsg(sock, &msg, 0);
-#else
             int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
-#endif
+
             // Error occurred during receiving
             if (len < 0) {
                 ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
@@ -120,15 +78,6 @@ static void udp_server_task(void *pvParameters)
                 // Get the sender's ip address as string
                 if (source_addr.ss_family == PF_INET) {
                     inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
-#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
-                    for ( cmsgtmp = CMSG_FIRSTHDR(&msg); cmsgtmp != NULL; cmsgtmp = CMSG_NXTHDR(&msg, cmsgtmp) ) {
-                        if ( cmsgtmp->cmsg_level == IPPROTO_IP && cmsgtmp->cmsg_type == IP_PKTINFO ) {
-                            struct in_pktinfo *pktinfo;
-                            pktinfo = (struct in_pktinfo*)CMSG_DATA(cmsgtmp);
-                            ESP_LOGI(TAG, "dest ip: %s\n", inet_ntoa(pktinfo->ipi_addr));
-                        }
-                    }
-#endif
                 } else if (source_addr.ss_family == PF_INET6) {
                     inet6_ntoa_r(((struct sockaddr_in6 *)&source_addr)->sin6_addr, addr_str, sizeof(addr_str) - 1);
                 }
@@ -157,25 +106,4 @@ static void udp_server_task(void *pvParameters)
         }
     }
     vTaskDelete(NULL);
-}
-
-void app_main(void)
-{
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
-
-#ifdef CONFIG_EXAMPLE_IPV4
-    xTaskCreate(udp_server_task, "udp_server", 4096, (void*)AF_INET, 5, NULL);
-#endif
-#ifdef CONFIG_EXAMPLE_IPV6
-    xTaskCreate(udp_server_task, "udp_server", 4096, (void*)AF_INET6, 5, NULL);
-#endif
-
 }
