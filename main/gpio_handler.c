906f6d3abb83f1546e83dcdd2a5e04e294e8f460
Severin Lochschmidt <seviron53@gmail.com>
2023-09-21 20:58:52 +0200

    Fix gpio

--- main/gpio_handler.c
+++ main/gpio_handler.c
@@ -6,12 +6,16 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
+#include "hal/gpio_types.h"
 
 #define GPIO_BUTTON_PIN 12
+#define ESP_INTR_FLAG_DEFAULT 0
 
 TaskHandle_t* state_task = NULL;
 
 void IRAM_ATTR button_isr() {
+    ESP_LOGE("isr", "GPIO ISR Triggered");
     xTaskNotifyGive(*state_task);
 }
 
@@ -20,5 +24,7 @@ void setup_button_handler(TaskHandle_t* state_task_handle){
     state_task = state_task_handle;
     gpio_reset_pin(GPIO_BUTTON_PIN);
     gpio_set_direction(GPIO_BUTTON_PIN, GPIO_MODE_INPUT);
-    gpio_isr_handler_add(GPIO_BUTTON_PIN, button_isr, (void*) GPIO_BUTTON_PIN);
+    gpio_set_intr_type(GPIO_BUTTON_PIN, GPIO_INTR_NEGEDGE);
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(GPIO_BUTTON_PIN, button_isr, NULL);
 }
